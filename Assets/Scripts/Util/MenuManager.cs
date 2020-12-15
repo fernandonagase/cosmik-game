b83918055c755b84612d3289ec788fa4f70d592b
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-15 20:56:43 -0300

    feat: implement menu scene

---
+++ Assets/Scripts/Util/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _creditsPanel = null;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Credits()
+    {
+        _creditsPanel.SetActive(!_creditsPanel.activeSelf);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
