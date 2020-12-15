3aed6b12104a8f30fd22d99b441d4d5816599efa
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-15 16:57:28 -0300

    feat: implement enemy spawner

---
+++ Assets/Scripts/Spawner/BaseSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class BaseSpawner : MonoBehaviour
+{
+    protected float spawnClock;
+    protected float elapsedTime;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= spawnClock)
+        {
+            Spawn();
+            elapsedTime -= spawnClock;
+        }
+    }
+
+    protected abstract void Spawn();
+}
---
+++ Assets/Scripts/Spawner/EnemySpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : BaseSpawner
+{
+    [SerializeField] private GameObject _enemyPrefab = null;
+
+    private void Start()
+    {
+        spawnClock = 2f;
+    }
+
+    protected override void Spawn()
+    {
+        Instantiate(_enemyPrefab, transform.position, transform.rotation);
+    }
+}
