b8c0b5b3c0d9e4999962be5e030572078f128106
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-15 20:12:27 -0300

    feat: implement pause menu

---
+++ Assets/Scripts/Util/AudioController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer = null;
+
+    public void SetMasterVolume(float value)
+    {
+        mixer.SetFloat("masterVol", value);
+    }
+
+    public void SetBgmVolume(float value)
+    {
+        mixer.SetFloat("bgmVol", value);
+    }
+
+    public void SetSfxVolume(float value)
+    {
+        mixer.SetFloat("sfxVol", value);
+    }
+}
---
+++ Assets/Scripts/Util/PauseManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool isPaused;
+
+    [SerializeField]
+    private GameObject pausePanel = null;
+
+    public void Pause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0.0f : 1.0f;
+        pausePanel.SetActive(isPaused);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
