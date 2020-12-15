b54299d1e5ee3984e0dad38abfca9d5051fdf398
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-15 18:28:10 -0300

    refactor: spawn enemies in random x positions

--- Assets/Scripts/Spawner/EnemySpawner.cs
+++ Assets/Scripts/Spawner/EnemySpawner.cs
@@ -3,14 +3,27 @@
 public class EnemySpawner : BaseSpawner
 {
     [SerializeField] private GameObject _enemyPrefab = null;
+    private Camera _mainCam;
+
+    private Vector3 _lowerLeftLimit;
+    private Vector3 _upperRightLimit;
 
     private void Start()
     {
         spawnClock = 2f;
+
+        _mainCam = Camera.main;
+        _lowerLeftLimit = _mainCam.ScreenToWorldPoint(Vector3.zero);
+        _upperRightLimit = _mainCam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
     }
 
     protected override void Spawn()
     {
-        Instantiate(_enemyPrefab, transform.position, transform.rotation);
+        float horizontalPosition = Random.Range(_lowerLeftLimit.x, _upperRightLimit.x);
+        Instantiate(
+            _enemyPrefab, 
+            new Vector3(horizontalPosition, transform.position.y, 0),
+            transform.rotation
+        );
     }
 }
