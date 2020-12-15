435986f36366bec45a74f4de326ac0fb901deace
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-15 16:29:23 -0300

    feat: implement constant player shooting

---
+++ Assets/Scripts/Behaviour/PlayerBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private float SHOOT_CLOCK = 1f;
+    private float BLAST_FORCE = 256f;
+
+    private float _elapsedTime;
+
+    private GameObject _firespot;
+    [SerializeField]
+    private GameObject _blastPrefab = null;
+
+    private void Start()
+    {
+        _firespot = GameObject.FindWithTag("PlayerFirespot");
+    }
+
+    private void FixedUpdate()
+    {
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime >= SHOOT_CLOCK)
+        {
+            Shoot();
+            _elapsedTime -= SHOOT_CLOCK;
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject newBlast = Instantiate(
+            _blastPrefab,
+            _firespot.transform.position,
+            transform.rotation
+        );
+        newBlast.GetComponent<Rigidbody2D>()
+            .AddForce(Vector2.up * BLAST_FORCE);
+
+        Destroy(newBlast, 5);
+    }
+}
