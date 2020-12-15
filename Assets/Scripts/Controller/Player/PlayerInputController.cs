fcef38aae94bcbea8dc9f2f3077e5eeba06d4000
Fernando Nagase <nagase.fernando@gmail.com>
2020-12-15 16:09:44 -0300

    feat: implement player movement

---
+++ Assets/Scripts/Controller/Player/PlayerInputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    private Camera _mainCam;
+
+    private void Start()
+    {
+        _mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            transform.position = (Vector2)_mainCam.ScreenToWorldPoint(touch.position);
+        }
+    }
+}
